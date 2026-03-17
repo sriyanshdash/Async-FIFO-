8dd07dc8a66f4de2a39e6af4518d1f25126e9acb
Sriyansh Dash <sdash@mqcomp8.marqueesemi.com>
2026-03-17 11:57:20 +0530

    Final Commit Draft #2

---
+++ verif/run/results/log/xcelium.d/run.lnx86.24.09.d/.timestamp.ts
@@ -0,0 +1,6 @@
+1773728002 /scratch/users/sdash/Async-FIFO-E/verif/tb/fifo_tb_top.sv
+1773728002 /scratch/users/sdash/Async-FIFO-E/rtl/fifo_top.sv
+1773728002 /scratch/users/sdash/Async-FIFO-E/rtl/wrptr_handler.sv
+1773728002 /scratch/users/sdash/Async-FIFO-E/rtl/rdptr_handler.sv
+1773728002 /scratch/users/sdash/Async-FIFO-E/rtl/fifo_memory.sv
+1773728002 /scratch/users/sdash/Async-FIFO-E/rtl/synchronizer.sv
